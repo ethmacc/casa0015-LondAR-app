4748a65505559e18c55afa3617593a6241ae8131
unknown <60006290+ethmacc@users.noreply.github.com>
2024-04-12 00:26:17 +0100

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
